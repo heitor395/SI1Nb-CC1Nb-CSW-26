27503bf83702c083c0a056d8b2317cd9f21f4c81
HeitorFadlalah <heitorfad30@gmail.com>
2026-05-13 22:36:01 -0300

    Add files via upload

---
+++ Aulas/Aula 14/javascript.js
@@ -0,0 +1,18 @@
+var x = 5;
+if (x > 5) {
+    console.log("x is greater than 5");
+} else if (x < 5) { 
+    console.log("x is less than or equal than 5");
+} else {
+    console.log("x is equal to 5"); 'x is equal to 5'
+}
+
+for(var i = 0; i<10; i+=2){
+    console.log(i);
+};
+
+var y = 0;
+while (y<10) {
+    console.log(y);
+    y++;
+};
